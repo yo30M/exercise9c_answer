4a4c82bdebe7f60f8cba6e6e924ed5106304829d
EC2 Default User <ec2-user@ip-172-31-37-43.ec2.internal>
2021-11-02 06:57:31 +0000

    feat: create Community-7

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   	get 'followings' => 'relationships#followings', as: 'followings'
   	get 'followers' => 'relationships#followers', as: 'followers'
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :groups, only: [:new, :index, :show, :create, :edit, :update]
+  
   get '/search', to: 'searches#search'
 end
